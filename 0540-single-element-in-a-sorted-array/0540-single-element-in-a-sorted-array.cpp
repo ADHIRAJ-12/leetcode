cd9e9cfe5d405f63b5ad242c378a4722469da128
Adhiraj <104266861+ADHIRAJ-12@users.noreply.github.com>
2022-12-17 20:20:23 +0530

    Time: 55 ms (58.41%), Space: 22.4 MB (56.61%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -14,16 +14,24 @@ public:
 //         }
 //         return ans;
         
-         int n=nums.size();
-        int s=0,e=n-1;
+//          int n=nums.size();
+//         int s=0,e=n-1;
         
-        while(s<e){
-            int mid=s+(e-s)/2;
-            if(mid%2==0 && nums[mid]==nums[mid+1] ||mid%2!=0 && nums[mid]==nums[mid-1])s=mid+1;
+//         while(s<e){
+//             int mid=s+(e-s)/2;
+//             if(mid%2==0 && nums[mid]==nums[mid+1] ||mid%2!=0 && nums[mid]==nums[mid-1])s=mid+1;
             
-            else e=mid;
-        }
-        return nums[s];
+//             else e=mid;
+//         }
+//         return nums[s];
         
+        
+        if(nums.size()==1)return nums[0];
+        
+        for(int i=0;i<nums.size();i=i+2){
+            if(nums[i]!=nums[i+1])return nums[i];
+        }
+        
+        return -1;
     }
 };
