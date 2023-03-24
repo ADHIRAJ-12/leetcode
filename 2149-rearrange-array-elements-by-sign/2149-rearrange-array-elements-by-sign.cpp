076e424c88ddbb09d7410101f896a772b03c657b
Adhiraj <104266861+ADHIRAJ-12@users.noreply.github.com>
2023-03-24 20:16:04 +0530

    Time: 225 ms (80.12%), Space: 123.8 MB (95.95%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -3,19 +3,50 @@ public:
     vector<int> rearrangeArray(vector<int>& nums) {
                 
         int n=nums.size();
-        vector<int>positive;
-        vector<int>negative;
         vector<int>ans(n);
+        int even=0,odd=1;
         
         for(int i=0;i<n;i++){
-            if(nums[i]>=0)positive.push_back(nums[i]);
-            else negative.push_back(nums[i]);
+            if(nums[i]>=0){
+                ans[even]=nums[i];
+                even+=2;
+            }
+            else{
+                ans[odd]=nums[i];
+                odd+=2;
+            }
         }
-        
-        for(int i=0;i<n/2;i++){
-            ans[2*i]=positive[i];
-           ans[2*i+1]=negative[i];           
-        }
-        return ans;
-    }
+           return ans;
+            }
 };
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         int n=nums.size();
+//         vector<int>positive;
+//         vector<int>negative;
+//         vector<int>ans(n);
+        
+//         for(int i=0;i<n;i++){
+//             if(nums[i]>=0)positive.push_back(nums[i]);
+//             else negative.push_back(nums[i]);
+//         }
+        
+//         for(int i=0;i<n/2;i++){
+//             ans[2*i]=positive[i];
+//            ans[2*i+1]=negative[i];           
+//         }
+//         return ans;
