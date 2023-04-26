b7c1ae40611a9bd69860ede38342e1e054722458
Adhiraj <104266861+ADHIRAJ-12@users.noreply.github.com>
2023-04-26 10:22:56 +0530

    Time: 6 ms (28.08%), Space: 7.1 MB (88.99%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,37 +1,47 @@
 class Solution {
 
-void solve( vector<vector<int>> &ans,vector<int> &temp,vector<int>& nums,int index){
+// void solve( vector<vector<int>> &ans,vector<int> &temp,vector<int>& nums,int index){
     
-    int n=nums.size();
-    if(index==n){
-        ans.push_back(temp);
-        return;
-    }
+//     int n=nums.size();
+//     if(index==n){
+//         ans.push_back(temp);
+//         return;
+//     }
     
-    //include
-    temp.push_back(nums[index]);
-    solve(ans,temp,nums,index+1);
-    temp.pop_back();
+//     //include
+//     temp.push_back(nums[index]);
+//     solve(ans,temp,nums,index+1);
+//     temp.pop_back();
     
-    //exlude
-    solve(ans,temp,nums,index+1);
+//     //exlude
+//     solve(ans,temp,nums,index+1);
     
-}
-    
-    
-    
-    
-    
+// }
     
     
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         
-        vector<vector<int>> ans;
+        // vector<vector<int>> ans;
+        // vector<int>temp;
+        // solve(ans,temp,nums,0);
+        // return ans;
+        
+        //bitmasking solution 
+        int n=nums.size();
+        int count=1<<n;
+        vector<vector<int>>ans;
+        for(int mask=0;mask<count;mask++){
         vector<int>temp;
-        solve(ans,temp,nums,0);
+        for(int i=0;i<n;i++){
+            if((mask & (1<<i))!=0){
+                temp.push_back(nums[i]);
+            }
+        }
+            ans.push_back(temp);
+     }  
         return ans;
-
+            
     }
 };
 
