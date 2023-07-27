027c62dad9853ef33acb34bfbef97dee1ddf1f6c
Adhiraj <104266861+ADHIRAJ-12@users.noreply.github.com>
2023-07-27 10:14:03 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (62.31%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -2,31 +2,26 @@ class Solution {
 public:
     bool stoneGame(vector<int>& nums) {
      
-//         int n=nums.size(),a=0,b=0;
-//         int i=0,j=n-1;
-//         while(i<=j){
-//             if(nums[i]>nums[j]){
-//                 a+=nums[i];
-//                 i++;
-//             }
-//             else{
-//                 a+=nums[j];
-//                 j--;
-//             }
+        int n=nums.size(),a=0,b=0;
+        int i=0,j=n-1;
+        while(i<j){
+            if(nums[i]>nums[j]){
+                a+=nums[i];
+                b+=nums[j];
+                i++;
+                j--;
+            }
+          
+            else{
+                a+=nums[j];
+                b+=nums[i];
+                i++;
+                j--;
+            }
             
-//             if(nums[i]>nums[j]){
-//                 b+=nums[i];
-//                 i++;
-//             }
-//             else{
-//                 b+=nums[j];
-//                 j--;
-//             }
+        }
+        if(a>b)return true;
+        else return false;
             
-//         }
-//         if(a>b)return true;
-//         else return false;
-        return true;
-        
     }
 };
