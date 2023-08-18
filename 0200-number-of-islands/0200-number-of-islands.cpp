77b2feb9d97db8de855662ffa377ab2efdfacb45
Adhiraj <104266861+ADHIRAJ-12@users.noreply.github.com>
2023-08-18 13:03:12 +0530

    Time: 42 ms (46.07%), Space: 22.5 MB (11.78%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,36 +1,55 @@
-
 class Solution {
 public:
     
-    void dfs(vector<vector<char>>& grid, int i, int j, int m, int n){
+    
+    void bfs(int row,int col,vector<vector<char>>& grid,vector<vector<int>>& vis){
+        int n=grid.size();
+        int m=grid[0].size();
+        vis[row][col]=1;
+        queue<pair<int,int>>q;
+        q.push({row,col});
+        vector<vector<int>> dir = {{0,1},{0,-1},{-1,-0},{1,0}};
         
-        if (i<0 || j<0 || i==m || j==n || grid[i][j]=='0') return;
+        while(!q.empty()){
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            
+            for(int d=0;d<dir.size();++d){
+            
+            int nrow=row+dir[d][0];
+            int ncol=col+dir[d][1];
+            
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && grid[nrow][ncol]=='1'){
+                    vis[nrow][ncol]=1;
+                    q.push({nrow,ncol});
+                }
+                
+            }
+
+        }
         
-        grid[i][j]='0';
-        dfs(grid,i+1,j,m,n);
-        dfs(grid,i,j+1,m,n);
-        dfs(grid,i-1,j,m,n);
-        dfs(grid,i,j-1,m,n);
-        return;
         
         
     }
+    
     
     
     
     int numIslands(vector<vector<char>>& grid) {
-        int count =0;
-        int m = grid.size();
-        int n = grid[0].size();
-        for (int i=0; i<m; i++){
-            for (int j=0; j<n; j++){
-                if (grid[i][j]=='1'){
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1' && !vis[i][j]){
                     count++;
-                   dfs(grid,i,j,m,n);
+                    bfs(i,j,grid,vis);
                 }
             }
         }
         return count;
+        
     }
-    
 };
