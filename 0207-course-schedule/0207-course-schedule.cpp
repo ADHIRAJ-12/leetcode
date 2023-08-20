4fa19b2ff72b4570f634218d2b0b16e41e859c31
Adhiraj <104266861+ADHIRAJ-12@users.noreply.github.com>
2023-08-20 20:45:38 +0530

    Time: 16 ms (92.86%), Space: 13.1 MB (92.90%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,43 +1,40 @@
 class Solution {
 public:
-    
-    bool iscyclic(int node,vector<int> adj[],vector<int> &visited,vector<int> &dfsvisited){
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
         
-        visited[node]=true;
-        dfsvisited[node]=true;
-        
-        for(auto nbr:adj[node]){
-            if(!visited[nbr]){
-                if(iscyclic(nbr,adj,visited,dfsvisited))return true;
-            }
-            else if(dfsvisited[nbr]==1)return true;
-        }
-        dfsvisited[node]=2;
-        return false;
-    }
-    
-    
-    
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        
-        
-        //if there exists a cycle in the graph then schedule is not possible
-        //so basically we have to check if cycle exists or not
-        
-        
-        vector<int>adj[numCourses];
-
-        for(int i=0;i<prerequisites.size();i++)
+        //topo sort logic
+        vector<int>adj[n];
+        for(int i=0;i<prerequisites.size();i++){
             adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        }
         
-        vector<int>visited(numCourses,0);
-        vector<int>dfsvisited(numCourses,0);
-
-        for(int i=0;i<numCourses;i++){
-            if(!visited[i]){
-                if(iscyclic(i,adj,visited,dfsvisited))return false;
+        vector<int>ans;
+        vector<int>indegree(n,0);
+        queue<int>q;
+        
+        for(int i=0;i<n;i++){
+            for(auto node:adj[i]){
+                indegree[node]++;
             }
         }
-        return true;
+        
+        for(int i=0;i<n;i++){
+                if(indegree[i]==0)q.push(i);
+        }
+        
+        
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+            
+            for(auto i:adj[node]){
+                indegree[i]--;
+                if(indegree[i]==0)q.push(i);
+            }
+        }
+        
+        return ans.size()==n;
+        
     }
 };
