9cfb9f1ff07289bb0fd38f20076aba5f57d55015
Adhiraj <104266861+ADHIRAJ-12@users.noreply.github.com>
2023-08-24 13:40:30 +0530

    Time: 22 ms (63.74%), Space: 16.5 MB (10.92%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,41 +1,46 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-
-        vector<int>dist(n,-1);
-        vector<int>temp(n);
-        dist[src]=0;
-
-        for(int i=0;i<=k;i++){
-            temp=dist;
-
-            for(int j=0;j<flights.size();j++){
-                int u=flights[j][0];
-                int v=flights[j][1];
-                int w=flights[j][2];
-
-                if(dist[u]==-1)continue;
-
-                if(temp[v]==-1 ||dist[u]+w<temp[v])temp[v]=dist[u]+w;
-
-            }
-            dist=temp;
-        }
-       
-
-      return dist[dst];
-
-	}
-};
-	// int INF=1e9;
-		// vector<vector<int>> dp(k+2,vector<int>(n,INF));
-		// dp[0][src]=0;
-		// for(int i=1;i<=k+1;i++){
-		// 	dp[i][src]=0;
-		// 	for(auto x:flights){
-		// 		dp[i][x[1]]=min(dp[i][x[1]],dp[i-1][x[0]]+x[2]);
-		// 	}
-		// }
-		// return dp[k+1][dst]>=INF ? -1:dp[k+1][dst];
         
+        vector<int>dist(n,1e9);
+        dist[src]=0;
+        vector<vector<int>>adj[n];
+        
+        for(int i=0;i<flights.size();i++){
+            adj[flights[i][0]].push_back({flights[i][1],flights[i][2]});
+        }
 
+//k,price,src
+        queue<pair<int,pair<int,int> > >mini;
+        mini.push({0,{0,src}});
+        
+        while(!mini.empty()){
+            
+            int count=mini.front().first;
+            int price=mini.front().second.first;
+            int node=mini.front().second.second;
+            mini.pop();
+            
+            // if(node==dst &&count<=k+1 )return price;
+            
+            count++;
+            for(auto i:adj[node]){
+                
+                int wt=i[1];
+                int nbr=i[0];
+                if(price+wt<dist[nbr] &&count<=k+1)  {
+                    int newprice=price+wt;
+                    dist[nbr]=newprice;
+                    mini.push({count,{newprice,nbr}});
+                }
+            
+         }
+
+            
+        }
+        if(dist[dst]==1e9)return -1;
+        else return dist[dst];
+        
+        
+    }
+};
