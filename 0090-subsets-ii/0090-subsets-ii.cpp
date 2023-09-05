e3769b030a3f1bb10c98f4f118efd63a9a510fa4
Adhiraj <104266861+ADHIRAJ-12@users.noreply.github.com>
2023-09-05 10:14:07 +0530

    Time: 2 ms (67.84%), Space: 7.4 MB (76.43%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,36 +1,47 @@
 class Solution {
 public:
     
-    void f(int ind,vector<int>&nums,vector<int>&temp,vector<vector<int>>&ans){
+//     void f(int ind,vector<int>&nums,vector<int>&temp,vector<vector<int>>&ans){
         
-        if(ind==nums.size()){
+//         if(ind==nums.size()){
+//             ans.push_back(temp);
+//             return;
+//         }
+        
+//         temp.push_back(nums[ind]);
+//         f(ind+1,nums,temp,ans);
+//         temp.pop_back();
+//         f(ind+1,nums,temp,ans);
+        
+//     }
+    
+     void f(int ind,vector<int>&nums,vector<int>&temp,vector<vector<int>>&ans){
+        
             ans.push_back(temp);
-            return;
-        }
         
-        temp.push_back(nums[ind]);
-        f(ind+1,nums,temp,ans);
-        temp.pop_back();
-        f(ind+1,nums,temp,ans);
-
-        
+         for(int i=ind;i<nums.size();i++){
+             if(i>ind && nums[i]==nums[i-1])continue;
+            temp.push_back(nums[i]);
+            f(i+1,nums,temp,ans);
+            temp.pop_back();             
+             
+         }
         
     }
-    
-    
     
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>>ans;
         vector<int>temp;
+        sort(nums.begin(),nums.end());
         f(0,nums,temp,ans);
         
-        for(int i=0;i<ans.size();i++)sort(ans[i].begin(),ans[i].end());
-        sort(ans.begin(),ans.end());
+        // for(int i=0;i<ans.size();i++)sort(ans[i].begin(),ans[i].end());
+        // sort(ans.begin(),ans.end());
         
-        set<vector<int>>st;
-        for(auto i:ans)st.insert(i);
-        ans.clear();
-        for(auto i:st)ans.push_back(i);
+        // set<vector<int>>st;
+        // for(auto i:ans)st.insert(i);
+        // ans.clear();
+        // for(auto i:st)ans.push_back(i);
         return ans;
         
     }
