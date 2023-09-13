e7dceb40fbf695783d5416ef26151a5dddca873c
Adhiraj <104266861+ADHIRAJ-12@users.noreply.github.com>
2023-09-13 13:46:18 +0530

    Time: 4 ms (87.77%), Space: 17.4 MB (46.50%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -18,34 +18,33 @@ public:
 
     int res=0;
     queue<pair<TreeNode*,int>>q;
-    q.push({root,1});   //node and index pushed
+    q.push({root,0});   //node and index pushed
 
 
      while(!q.empty()){
+         
          int size=q.size();
-
          int mini=q.front().second; ///min index at that level
-         int mn,mx;
+         int first,last;
 
          for(int i=0;i<size;i++){
              
              //for preventing overflow, it changes the curr index of each level
              //by decresing min index
-             int curr=q.front().second-mini+1;
-
-             TreeNode*temp=q.front().first;
+            TreeNode*temp=q.front().first;
+            int curr=q.front().second-mini;
              q.pop();
 
 
-             if(i==0)mn=curr;
-             if(i==size-1)mx=curr;
+             if(i==0)first=curr;
+             if(i==size-1)last=curr;
 
 
-             if(temp->left!=NULL)q.push({temp->left,2LL*curr});
-             if(temp->right!=NULL)q.push({temp->right,2LL*curr +1});  
+             if(temp->left!=NULL)q.push({temp->left,2LL*curr+1});
+             if(temp->right!=NULL)q.push({temp->right,2LL*curr +2});  
 
          }
-         res=max(res,mx-mn+1);
+         res=max(res,last-first+1);
      }
      return res;
     
