0dcf63b81d67216bbd2bbcf5c560a70ede035503
Adhiraj <104266861+ADHIRAJ-12@users.noreply.github.com>
2023-09-29 10:38:51 +0530

    Time: 101 ms (98.18%), Space: 96.8 MB (22.05%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.cpp
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -8,19 +8,13 @@ public:
         }
         return true;
     }
-     bool dec(vector<int>& nums){
-        
-         reverse(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i+1]<nums[i])return false;
-        }
-        return true;
-    }
+ 
     
     bool isMonotonic(vector<int>& nums) {
       
         if (inc(nums))return true;
-        return dec(nums)?true:false;
+         reverse(nums.begin(),nums.end());
+        return inc(nums)?true:false;
         
     }
 };
