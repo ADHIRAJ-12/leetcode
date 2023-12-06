379c73f69386cd17eef2f0659200e1f03900e03d
Adhiraj <104266861+ADHIRAJ-12@users.noreply.github.com>
2023-12-06 08:15:00 +0530

    Time: 3 ms (83.87%), Space: 14.3 MB (49.89%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,111 +1,34 @@
 class Solution {
 public:
-    
-//   int getPivot(vector<int>& arr,int n)
-// {
-// int s=0;
-//     int e=n-1;
-//     int mid= s+(e-s)/2;
-//     while(s<e){
-//         if(arr[mid]>=arr[0]){
-//             s=mid+1;
-//         }
-//         else{
-//             e=mid;
-//         }
-//         mid= s+(e-s)/2;
-//     }
-//     return s;
-// }
-    
-    
-// bool binarySearch(vector<int>& arr, int s,int e, int key) {
-
-//     int start = s;
-//     int end = e;
-
-//     int mid = start + (end-start)/2;
-
-//     while(start <= end) {
-
-//         if(arr[mid] == key) {
-//             return true;
-//         }
-
-        
-//         if(key > arr[mid]) {
-//             start = mid + 1;
-//         }
-//         else{ 
-//             end = mid - 1;
-//         }
-
-//         mid = start + (end-start)/2;
-//     }
-    
-//     return false;
-// }
-    
-    
-    bool search(vector<int>& nums, int target) {
-    //     int n=nums.size();
-    //     int pivot=getPivot(nums,n);
-    //     if (target>=nums[pivot]&&target<=nums[n-1]){
-    //     return binarySearch(nums,pivot,n-1,target);
-    // }
-    // else{
-    //     return binarySearch(nums,0,pivot-1,target);
-    // }
-       
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-            
-        int s=0,e=n-1;
+    bool search(vector<int>& arr, int k) {
+        int n=arr.size();
+    int s=0,e=n-1;
+    while(s<=e){
         int mid=s+(e-s)/2;
-        
-        while(s<=e){
-            if(nums[mid]==target)return true;
-            
-            else if(target>nums[mid]) {s=mid+1;}
-            
-            else {e=mid-1;}
-            
-             mid=s+(e-s)/2;
+        if(arr[mid]==k)return true;
+        if(arr[s]==arr[mid] && arr[mid]==arr[e]){
+            s++;
+            e--;
+            continue;
+        }
+
+            //left part sorted
+        if(arr[s]<=arr[mid]){
+
+                if(arr[s]<=k && arr[mid]>=k )e=mid-1;
+                else s=mid+1;
 
         }
-        return false;
+        //right part sorted
+        else{
+
+            if(arr[mid]<=k && arr[e]>=k)s=mid+1;
+            else e=mid-1;
+
+        }
+
+
+    }
+return false;
     }
 };
-
-
-
-
-
-
-
-
-//         //linear search
-//         sort(nums.begin(),nums.end());
-//         for(int i=0;i<nums.size();i++){
-//             if(nums[i]==target) return true;
-//         }
-//         return false;
-        
-        //binary search
-//         sort(nums.begin(),nums.end());
-//         int n=nums.size();
-            
-//         int s=0,e=n-1;
-//         int mid=s+(e-s)/2;
-        
-//         while(s<=e){
-//             if(nums[mid]==target)return true;
-            
-//             else if(target>nums[mid]) {s=mid+1;}
-            
-//             else {e=mid-1;}
-            
-//              mid=s+(e-s)/2;
-
-//         }
-//         return false;
