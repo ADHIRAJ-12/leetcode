5c31ce9530d35438c28a9a161902758cefe115e1
Adhiraj <104266861+ADHIRAJ-12@users.noreply.github.com>
2023-12-07 07:56:15 +0530

    Time: 22 ms (39.94%), Space: 22.6 MB (91.68%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& nums) {
-//         unordered_map<int,int>m;
-//         int n=nums.size();
-//         int ans;
+    int singleNonDuplicate(vector<int>& arr) {
+       
+        int n=arr.size();
+	if(n==1)return arr[0];
+
+	if(arr[0]!=arr[1])return arr[0];
+	if(arr[n-1]!=arr[n-2])return arr[n-1];
+
+
+	int s=1,e=n-2;
+	while(s<=e){
+
+		int mid=s+(e-s)/2;
+		
+		if(arr[mid]!=arr[mid+1] && arr[mid]!=arr[mid-1])return arr[mid];
+//left half
+
+		if(mid%2==1 &&arr[mid-1]==arr[mid] ||
+		   mid%2==0 && arr[mid+1]==arr[mid]){
+					s=mid+1;
+		   }
+		   else{
+			   //right half
+			   e=mid-1;
+		   }
+
+
+	}
+
+return  -1;
         
-//         for(int i=0;i<n;i++){
-//             m[nums[i]]++;
-//         }
-        
-//         for(auto i:m){
-//             if(i.second==1)ans=i.first;
-//         }
-//         return ans;
-        
-//         int n=nums.size();
-//         int s=0,e=n-1;
-        
-//         while(s<e){
-//             int mid=s+(e-s)/2;
-//             if(mid%2==0 && nums[mid]==nums[mid+1] ||mid%2!=0 && nums[mid]==nums[mid-1])s=mid+1;
-            
-//             else e=mid;
-//         }
-//         return nums[s];
-        
-        
-        if(nums.size()==1)return nums[0];
-        
-        for(int i=0;i<nums.size();i=i+2){
-            if(nums[i]!=nums[i+1])return nums[i];
-        }
-        
-        return -1;
     }
 };
