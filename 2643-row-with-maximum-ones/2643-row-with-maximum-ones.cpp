36b9840104c046fb3ff02f59e7213ae0c3bdafc8
Adhiraj <104266861+ADHIRAJ-12@users.noreply.github.com>
2023-12-17 12:43:21 +0530

    Time: 124 ms (25.42%), Space: 66.3 MB (5.14%) - LeetHub

--- 2643-row-with-maximum-ones/2643-row-with-maximum-ones.cpp
+++ 2643-row-with-maximum-ones/2643-row-with-maximum-ones.cpp
@@ -1,37 +1,18 @@
 class Solution {
 public:
-    
-    int lowerBound(vector<int> arr, int n, int x) {
-
-	int ans=n;
-
-	int s=0,e=n-1;
-	while(s<=e){
-
-		int mid=s+(e-s)/2;
-		if(arr[mid]>=x){
-			ans=mid;
-			e=mid-1;
-		}
-		else s=mid+1;
-
-	}
-	return ans;
-}
-
-    
     vector<int> rowAndMaximumOnes(vector<vector<int>>& matrix) {
-         for(auto &row:matrix){
-            sort(row.begin(), row.end());
-        }
-    int count=0,index=0,n=matrix.size(),m=matrix[0].size();
+          for(auto &row:matrix)sort(row.begin(), row.end());
+        
+    int count=0,index=-1,n=matrix.size(),m=matrix[0].size();
     for(int i=0;i<n;i++){
-       int ones=m-lowerBound(matrix[i],m,1);
+        vector<int>arr=matrix[i];
+        int ones=m-(lower_bound(arr.begin(),arr.end(),1)-arr.begin());
         if(ones>count){
             count=ones;
             index=i;
         }
     }
+        if(index==-1)index=0;
          return {index,count};
     }
 };
