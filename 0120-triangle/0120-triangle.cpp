dabdf2a1d184b90efac657e677a17b621c036ec9
Adhiraj <104266861+ADHIRAJ-12@users.noreply.github.com>
2024-02-08 00:13:43 +0530

    Time: 0 ms (100.00%), Space: 11.4 MB (19.03%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -13,25 +13,51 @@ public:
 //             return dp[i][j]= min(down,daig);
 //         }
     
-        int minimumTotal(vector<vector<int>>& triangle) {
+    
+//         int minimumTotal(vector<vector<int>>& triangle) {
+
+//              int n=triangle.size();
+//              // vector<vector<int>>dp(n,vector<int>(n,-1));
+//              // return f(0,0,triangle,n,dp);
+//             vector<vector<int>>dp(n,vector<int>(n,0));
+//             for(int i=0;i<n;i++){
+//                  dp[n-1][i]=triangle[n-1][i];
+//             }
+           
+            
+//             for(int i=n-2;i>=0;i--){
+//                 for(int j=0;j<=i;j++){
+//                      int down=triangle[i][j]+dp[i+1][j];
+//                      int daig=triangle[i][j]+dp[i+1][j+1];
+                    
+//                     dp[i][j]=min(down,daig);
+//                 }
+//             }
+//             return dp[0][0];
+//         }
+    
+    int minimumTotal(vector<vector<int>>& triangle) {
 
              int n=triangle.size();
-             // vector<vector<int>>dp(n,vector<int>(n,-1));
-             // return f(0,0,triangle,n,dp);
-            vector<vector<int>>dp(n,vector<int>(n,0));
+      
+            vector<int>dp(n,0);
             for(int i=0;i<n;i++){
-                 dp[n-1][i]=triangle[n-1][i];
+                 dp[i]=triangle[n-1][i];
             }
            
             
             for(int i=n-2;i>=0;i--){
+                 vector<int>temp(n,0);
                 for(int j=0;j<=i;j++){
-                     int down=triangle[i][j]+dp[i+1][j];
-                     int daig=triangle[i][j]+dp[i+1][j+1];
+                     int down=triangle[i][j]+dp[j];
+                     int daig=triangle[i][j]+dp[j+1];
                     
-                    dp[i][j]=min(down,daig);
+                    temp[j]=min(down,daig);
                 }
+                dp=temp;
             }
-            return dp[0][0];
+        
+            return dp[0];
         }
+    
 };
