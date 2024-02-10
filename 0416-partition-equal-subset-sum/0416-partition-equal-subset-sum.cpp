519f54b3841b8c45e56da5c9ffff199940adb0b8
Adhiraj <104266861+ADHIRAJ-12@users.noreply.github.com>
2024-02-11 00:41:00 +0530

    Time: 142 ms (77.40%), Space: 97.5 MB (29.56%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,104 +1,30 @@
 class Solution {
 public:
     
-//     bool solve(int index,vector<int>& nums,int N,int target){
+    bool solve(int ind,int k,vector<int>&nums,vector<vector<int>>&dp){
         
-//          if(index>=N || target<0) return false;
-//         if(target==0) return true;
+        if(k==0)return true;
+        if(ind==0)return nums[0]==k;
         
-//         int include=solve(index+1,nums,N,target-nums[index]);
-//         int exclude=solve(index+1,nums,N,target-0);
+        if(dp[ind][k]!=-1)return dp[ind][k];
         
-//         return include or exclude;
+        bool nottake=solve(ind-1,k,nums,dp);
+        bool take=false;
+        if(nums[ind]<=k)take=solve(ind-1,k-nums[ind],nums,dp);
         
-//     }
-    
-    
-//       bool solveMem(int index,vector<int>& nums,int N,int target,vector<vector<int>> &dp){
+        return dp[ind][k]=take||nottake;
         
-//          if(index>=N || target<0) return false;
-//         if(target==0) return true;
-//           if(dp[index][target]!=-1) return dp[index][target];
-        
-//         int include=solveMem(index+1,nums,N,target-nums[index],dp);
-//         int exclude=solveMem(index+1,nums,N,target-0,dp);
-        
-//         return dp[index][target]= include or exclude;
-        
-//     }
-    
-    
-//     bool solveTab(int N,vector<int>& nums,int total){
-    
-//         vector<vector<int>>dp(N+1,vector<int>(total+1,0));
-
-//         for(int i=0;i<=N;i++){
-//             dp[i][0]=1;
-            
-//         }      
-//         for(int index=N-1;index>=0;index--){
-//             for(int target=0;target<=total/2;target++){
-                
-//                 bool include=0;
-//                 if(target-nums[index]>=0)
-//                  include=dp[index+1][target-nums[index]];
-            
-//                 bool exclude=dp[index+1][target];
-
-//                 dp[index][target]= include or exclude;
-//             }
-//         }
-//         return dp[0][total/2];
-//     }
-    
-    
-    
-    
-      bool solveSpace(int N,vector<int>& nums,int total){
-    
-        vector<int>curr(total+1,0);       
-         vector<int>next(total+1,0);
-
-          curr[0]=1;
-          next[0]=1;
-     
-        for(int index=N-1;index>=0;index--){
-            for(int target=0;target<=total/2;target++){
-                
-                bool include=0;
-                if(target-nums[index]>=0)
-                 include=next[target-nums[index]];
-            
-                bool exclude=next[target];
-
-                curr[target]= include or exclude;
-            }
-            next=curr;
-        }
-        return next[total/2];
     }
     
     
     
-    
-    
-    
     bool canPartition(vector<int>& nums) {
-        int N=nums.size();
-        int total=0;
         
-        for(int i=0;i<N;i++){
-            total+=nums[i];
-        }
-        if(total&1) return 0;  //means odd
+        int sum=0,n=nums.size();
+        for(int i=0;i<n;i++)sum+=nums[i];
+        if(sum%2!=0)return false;
+        vector<vector<int>>dp(n,vector<int>((sum/2)+1,-1));
+        return solve(n-1,sum/2,nums,dp);
         
-        int target=total/2;
-        // return solve(0,nums,N,target);
-        // vector<vector<int>>dp(N,vector<int>(target+1,-1));
-        // return solveMem(0,nums,N,target,dp);
-        
-        // return solveTab(N,nums,total);
-                return solveSpace(N,nums,total);
-
     }
 };
